a18a6707101d2e28a704525e187ed0999b291a43
이요한 <@59.9.187.151>
2017-07-01 23:23:34 +0900

    강사님코드 가지고테스트시작

---
+++ RingDingDong/src/com/never/data/lee/server/ChatServer.java
@@ -0,0 +1,129 @@
+package com.never.data.lee.server;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class ChatServer {
+	private JFrame f;
+	private JTextField tfPort;
+	private JButton btnStart,btnStop;
+	private JTextArea taChatList ; 
+
+	
+	ServerThread serverThread;
+	
+	
+	ActionListener listener  = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			switch(e.getActionCommand()){
+			case "A" :
+				doStartServer();
+				break;
+			case "B" :
+				doStopServer();
+				break;
+			}
+		}
+	};
+	public  synchronized void addChatList(String msg){ //동기화 서버가 쓰레드기때문에 여러사람이한번에달라고하면 안되니까 동기화시켜야함 
+		taChatList.append(msg+"\n");
+		int length = taChatList.getText().length();//스트링에 랭쓰라는메소드가있다 
+		taChatList.setCaretPosition(length);//j스크롤 아니면  뷰포지션
+		//j텍스트에리어 셋 케얼 포지션 
+	}
+
+	void doStopServer(){
+		serverThread.stop();
+	}
+	
+	void doStartServer(){//메소드는 동사형태로 
+		String sPort = tfPort.getText();
+		int port = 12345;
+		try{
+			port = Integer.parseInt(sPort);
+		}catch(NumberFormatException e ){
+			addChatList("port오류  ["+e + "]");
+		}//오류가나도상관없지만  오류를 보여주자 
+
+		
+		serverThread = new ServerThread(this,port);
+		
+		
+		new Thread( serverThread ).start();//자기자신과 포트를넘김
+
+	}
+
+
+	public void settingsStopButton(){
+		btnStart.setEnabled(false);
+		btnStop.setEnabled(true);
+	}
+	public void settingsStartButton(){
+		btnStart.setEnabled(true);
+		btnStop.setEnabled(false);
+	}
+
+
+	public ChatServer() {
+		//채팅 서버  
+		f = new JFrame("서버창");
+		f.setBounds(1000,100,300,500);
+		JPanel npanel = new JPanel(new BorderLayout());
+		JPanel spanel = new JPanel(new GridLayout(1,2));
+
+
+		npanel.add(new JLabel("port"),BorderLayout.WEST);
+		tfPort= new JTextField("12345");
+		tfPort.setToolTipText("포트는 10000부터 65535 사이만입력하세요");
+		npanel.add(tfPort,BorderLayout.CENTER);
+
+		btnStart = new JButton("시작");
+		btnStart.setActionCommand("A");
+		btnStart.addActionListener(listener);
+
+		btnStop = new JButton("종료");
+		btnStop.setActionCommand("B");
+		btnStop.addActionListener(listener);
+		btnStop.setEnabled(false); //시작누르기전에는 종료를 누를수없다 
+
+		spanel.add(btnStart);
+		spanel.add(btnStop);
+
+		taChatList = new JTextArea() ;
+		taChatList.setEditable(false); //에디터블이랑 인에이블이랑차이가뭘까
+		//		taChatList.setEnabled(false);
+
+
+
+
+
+
+
+		f.add(npanel,BorderLayout.NORTH);
+		f.add(new JScrollPane(taChatList), BorderLayout.CENTER); //스크롤막대 생성 
+		f.add(spanel,BorderLayout.SOUTH);
+
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		f.setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ChatServer();
+	}
+
+}
---
+++ RingDingDong/src/com/never/data/lee/server/ServerThread.java
@@ -0,0 +1,143 @@
+package com.never.data.lee.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerThread implements  Runnable {
+
+
+	int port;
+	ChatServer server ;
+	boolean onAir ;
+	ServerSocket ss ;
+
+
+	ArrayList<Com> comList = new ArrayList<Com>();
+
+	public void sendUserList(Com com){ //유저리스트 
+		String userList= "";
+
+		StringBuilder sb = new StringBuilder();
+		for(Com c : comList){
+			sb.append(c.name).append(",");
+
+		}
+
+		userList = sb.toString();// 스트링빌더를 넣는다 
+		userList= userList.substring(0,userList.length()-1); //글자하나를 뺀다   서브스트링   메소드확인 해
+
+		com.sendMessage('L', userList);
+	}
+
+	public void exitCom(Com com ){//어레이리스트에서 객체제거하기 (나가기)
+		comList.remove(com); //리무브 (오브젝트 )
+	}
+
+	public void sendOneMessage(String name ,String pName , String msg){
+		Com com = null ; //에러떨어질까봐 
+		for(int  i = 0 ; i <comList.size() ; i ++){ //for: 은 무조건다돌고  ;; 은 돌다가나올수있으니 하다나올거면 ;;사용
+			com = comList.get(i);// 컴리스트에있는것을 . 겟 한다 (i번째껄)
+			if(com.name.equals(pName)){
+				
+				
+				break;
+			}
+		}
+		
+		if(com != null){
+			com.sendMessage('M',name+" 님의 귓속말 :" +msg);
+		}
+		
+		
+	}
+	
+	public void sendAllMessage(char protocol ,String msg){
+		//		for( int i = 0 ; i < comList.size() ; i++){
+		for( Com com : comList){   //디스를보내고 디스가맞으면 안보낸다 그러면 자기채팅은안나옴 
+
+			com.sendMessage(protocol,msg);
+		}
+	}
+
+	public void sendAllMessage(char protocol ,String msg,Com me){
+		//		for( int i = 0 ; i < comList.size() ; i++){
+		for( Com com : comList){   //디스를보내고 디스가맞으면 안보낸다 그러면 자기채팅은안나옴 
+			if(com !=me){
+				com.sendMessage(protocol,msg);
+			}
+		}
+	}
+
+
+
+	public ServerThread(ChatServer server ,int port) {
+		this.port = port;
+		this.server = server;
+	}	
+	public void stop(){
+		onAir = false ;
+		try{
+			ss.close();    //서버소켓을 강제로 종료 
+
+			for(Com com : comList ){
+				com.dos.writeUTF("서버끊김 ");
+				com.s.close();
+
+			}
+		}catch(IOException e ){
+			server.addChatList("서버가 강제로 종료되는도중오류"+e);
+		}
+		server.settingsStartButton();
+	}
+
+	public void run() {
+		//쓰레드 
+		ss =null; 
+
+		try {
+			ss = new ServerSocket(port);
+		} catch (IOException e) {
+			server.addChatList("port오류 "+e);
+			return; 
+		}
+
+		server.settingsStopButton();
+		server.addChatList("서버가 시작됨  Port : "+port);
+		onAir = true;
+
+		Socket s = null;
+		Com com ;
+
+		String remoteIP= "";
+		while (	onAir){ //오류가나면 서버를 죽이겠다 그러면 와일문 밖에 서버는안죽는다 하면 안쪽에 트라이캐치
+
+			try {
+
+				server.addChatList("접속대기중 ...");
+				s = ss.accept(); //될때마다 새로운 소켓이 생성됨 
+				remoteIP= s.getInetAddress().getHostAddress();
+				server.addChatList("접속완료 접속IP " +remoteIP );
+
+				//접속이이뤄진 부분 
+				com = new Com(server,this,s);
+				comList.add(com);
+
+				com.start(); //접근할 변수가없으면 그냥생성해서 스타트하면되는데 모아놓을려면 변수를 따로잡고 
+				// 변수를 리스트에 올려줘야한다 
+
+
+
+
+
+			} catch (IOException e) {
+				server.addChatList("서버접속 대기 실패"+e);
+			}
+
+		}
+		server.addChatList("서버가 종료됨 ");
+
+	}
+
+}
